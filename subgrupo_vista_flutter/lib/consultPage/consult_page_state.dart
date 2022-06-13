58de1b9e6b377cafbf58891811e90cb90e387b38
aliortiz2000 <alinesortiz2000@gmail.com>
2022-06-13 14:21:04 -0400

    Pasing-Data

--- subgrupo_vista_flutter/lib/consultPage/consult_page_state.dart
+++ subgrupo_vista_flutter/lib/consultPage/consult_page_state.dart
@@ -5,59 +5,13 @@ import 'consult_page.dart';
 abstract class ConsultPageState extends State<ConsultPage> {
   late ConsultPageController controller;
 
+  dynamic contex;
 
-  //Esto cambiara cuando haga la peticion a la Api
-  dynamic contex = {
-    "doctors": [
-      {
-        "name": "Carlos Landaeta",
-        "gender": "m",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Traumatologo",
-      },
-      {
-        "name": "Sofia Ruiz",
-        "gender": "f",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Dermatologia",
-      },
-      {
-        "name": "Alines Ortiz",
-        "gender": "f",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Urologia",
-      },
-      {
-        "name": "Marta Goncalves",
-        "gender": "f",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Radiologia",
-      },
-      {
-        "name": "Jose Flores",
-        "gender": "m",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Radiologia",
-      },
-      {
-        "name": "Pancracia Rausseo",
-        "gender": "f",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Gastroenterologia",
-      },
-      {
-        "name": "Rosmy Martinez",
-        "gender": "f",
-        "photo": "www.dsdsdsd.com/ds.jpg",
-        "specialty": "Cirugia",
-      }
-    ]
-  };
-
-  void setContex(String value) {
+  void setContex(value) {
     setState(() {
       //Aqui se le mandara el mensaje a PageController
       //Actualizar los ListView
+      contex = controller.get(value);
       //controller.get(value); --->Aqui se le pasa el mensaje a page controller
     });
   }
--- subgrupo_vista_flutter/lib/consultPage/doctor_consult_page_state.dart
+++ subgrupo_vista_flutter/lib/consultPage/doctor_consult_page_state.dart
@@ -3,28 +3,106 @@ import '../listView/doctor_list_view.dart';
 import '../pageController/doctor_controller.dart';
 import 'consult_page_state.dart';
 
-class DoctorConsultPageState extends ConsultPageState {
-  var dropdownValue= "Todos";
+class DoctorConsultPageState extends ConsultPageState 
+{
+  var dropdownValue = "Todos";
 
-  List<String> assignDrowpDownItems(){
+  List<String> assignDrowpDownItems() {
     List<String> assignDrowpDownItems = [
-        'Todos',
-        'Traumatologia',
-        'Pediatria',
-        'Cardiologia',
-        'Neurologia',
-        'Dermatologia',
-        'Urologia',
-        'Oftalmologia',
-        'Cirugia',
-        'Radiologia',
-        'Gastroenterologia'
+      'Todos',
+      'Traumatologia',
+      'Pediatria',
+      'Cardiologia',
+      'Neurologia',
+      'Dermatologia',
+      'Urologia',
+      'Oftalmologia',
+      'Cirugia',
+      'Radiologia',
+      'Gastroenterologia'
     ];
     return assignDrowpDownItems;
   }
 
-  Widget createFilter(){
+  @override
+  Widget build(BuildContext context) {
+    assignController();
 
+    //Barra de Filtro
+    var filter = createFilter();
+    var fbuilder = createFutureBuilder();
+
+    // Retorna el contenido de la pagina
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Doctores"),
+      ),
+      body: Column(
+        children: <Widget>[
+          filter,
+          Expanded(
+              child: fbuilder
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  void assignController() {
+    controller = DoctorConsultController();
+  }
+
+
+
+
+
+
+
+
+//////////////// Widgets individuales 
+
+//Widget para el Future
+Widget createFutureBuilder() {
+    return FutureBuilder(
+      future: contex,
+      builder: (
+        BuildContext context,
+        AsyncSnapshot<String> snapshot,
+      ) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(),
+              Visibility(
+                visible: snapshot.hasData,
+                child: Text(
+                  snapshot.data.toString(),
+                  style:
+                  const TextStyle(color: Colors.black, fontSize: 24),
+                ),
+              )
+            ],
+          );
+        } else if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return const Text('Error');
+          } else if (snapshot.hasData) {
+            return DoctorListView(contex);
+          } else {
+            return const Text('Empty data');
+          }
+        } else {
+          return Text('State: ${snapshot.connectionState}');
+        }
+      },
+    );
+}
+ 
+ //Widget para el Filtro
+  Widget createFilter() {
     return Container(
       margin: const EdgeInsets.all(5),
       padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 4),
@@ -35,12 +113,13 @@ class DoctorConsultPageState extends ConsultPageState {
         child: DropdownButton<String>(
           value: dropdownValue,
           onChanged: (String? newValue) {
-            setState(() {
+            setState(() {     
               dropdownValue = newValue!;
-              setContex(dropdownValue);
+              super.setContex(dropdownValue);///////////////////////////////////////////////super?
             });
           },
-          items: assignDrowpDownItems().map<DropdownMenuItem<String>>((String value) {
+          items: assignDrowpDownItems()
+              .map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
               child: Text(value),
@@ -54,35 +133,6 @@ class DoctorConsultPageState extends ConsultPageState {
             color: Colors.black,
           ),
         ),
-      ),
-    );
-  }
-
-  @override
-  void assignController(){
-    controller = DoctorConsultController();
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    assignController();
-
-    //Barra de Filtro
-    var filter = createFilter();
-
-    // Retorna el contenido de la pagina
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Doctores"),
-      ),
-      body: Column(
-        children: <Widget>[
-            filter,
-          Expanded(
-            child: DoctorListView(contex)
-          )
-        ],
       ),
     );
   }
